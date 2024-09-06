a214ca548d7b283b46b19a7582146958cd3048fa
Annurdien Rasyid <annurdien.dev@gmail.com>
2024-09-06 08:36:30 +0700

    Initial Commit

---
+++ whatsnap/whatsnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatsnapApp.swift
+//  whatsnap
+//
+//  Created by Annurdien Rasyid on 06/09/24.
+//
+
+import SwiftUI
+
+@main
+struct whatsnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
